fa2c5b79eb6dcf109bbb505b0ae72422ae1643bd
Marcelo <mabc@live.cl>
2020-03-29 01:23:44 -0300

    Versión 1.0 de producción

---
+++ build/precache-manifest.15b402ba564da87d570bd21af44157d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816f252562b67cb42764e97586a1a3ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "783b9be74cfdb2341112",
+    "url": "/static/css/2.d7f961f8.chunk.css"
+  },
+  {
+    "revision": "48292d3a00ac808a82e9",
+    "url": "/static/css/main.75e467cf.chunk.css"
+  },
+  {
+    "revision": "783b9be74cfdb2341112",
+    "url": "/static/js/2.d61f7089.chunk.js"
+  },
+  {
+    "revision": "3272418a5f03796eea40d8ddb7225b90",
+    "url": "/static/js/2.d61f7089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48292d3a00ac808a82e9",
+    "url": "/static/js/main.faee5f9d.chunk.js"
+  },
+  {
+    "revision": "755a97177770470877ca",
+    "url": "/static/js/runtime-main.c491b37a.js"
+  }
+]);
